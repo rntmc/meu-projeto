7e99770ac3311eb90e16b0ebcb30bdb2489aa3cd
Renato Carrino <carrino.renato@gmail.com>
2023-10-05 16:29:13 -0300

    Delete Backend directory

--- Backend/src/database/knex/migrations/20230920150424_createLinks.js
+++
@@ -1,9 +0,0 @@
-exports.up = knex => knex.schema.createTable("links", table => { // up: criar tabela
-  table.increments("id");
-  table.text("url").notNullable();// n pode ser nulo
-
-  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE"); // Deleta as tags deletadas as notas em cascata
-  table.timestamp("created_at").default(knex.fn.now());
-});
- 
-exports.down = knex => knex.schema.dropTable("links"); //down: deletar tabela
